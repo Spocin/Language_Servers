68db486c2141ba7aa578b3aeacd6f1a382282ebe
Michal Spocinski <s20461@pjwstk.edu.pl>
2021-05-03 21:29:41 +0200

    Modified Client_Start.bat

--- Artifacts/Client_Start.bat
+++ Artifacts/Client_Start.bat
@@ -1,1 +1,1 @@
-java --module-path "C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar Client.jar
+java --module-path "Insert path to JavaFx/lib here" --add-modules javafx.controls,javafx.fxml -jar Client.jar
